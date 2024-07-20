e9dde7037bfe514e22a6577093924b2f5e51bf61
Dave <davenlu1989@gmail.com>
2024-07-20 14:26:15 +0900

    chore: improve language

--- app/_components/popular.tsx
+++ app/_components/popular.tsx
@@ -14,40 +14,44 @@ import { AnimeList, AnimeListSkeleton } from "@/components/anime/anime-list";
 import { toast } from "sonner";
 
 export default function Popular() {
-  const [popularAnime, setPopularAnime] = useState<AnilistResult[]>([]);
-  const [loading, setLoading] = useState(true);
+    const [popularAnime, setPopularAnime] = useState<AnilistResult[]>([]);
+    const [loading, setLoading] = useState(true);
 
-  /**
-   * Fetches the popular anime data from the API and updates the state.
-   * Shows a toast error if there is an error fetching the data.
-   */
-  useEffect(() => {
-    async function fetchPopularAnime() {
-      try {
-        const response = await getPopularAnime();
-        setPopularAnime(response.results);
-      } catch (error) {
-        toast.error("Failed to load popular anime. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    }
+    /**
+     * Fetches the popular anime data from the API and updates the state.
+     * Shows a toast error if there is an error fetching the data.
+     */
+    useEffect(() => {
+        async function fetchPopularAnime() {
+            try {
+                const response = await getPopularAnime();
+                setPopularAnime(response.results);
+            } catch (error) {
+                toast.error(
+                    "Failed to load popular anime. Please try again later."
+                );
+            } finally {
+                setLoading(false);
+            }
+        }
 
-    fetchPopularAnime();
-  }, []);
+        fetchPopularAnime();
+    }, []);
 
-  return (
-    <div className="px-4 lg:px-8 py-6">
-      <div>
-        <h1 className="text-2xl font-medium">Popular Animes of All Time</h1>
-      </div>
-      {/* Display a loading skeleton if the data is still being loaded */}
-      {loading ? (
-        <AnimeListSkeleton type="default" />
-      ) : (
-        // Display the list of popular anime
-        <AnimeList type="default" list={popularAnime} />
-      )}
-    </div>
-  );
+    return (
+        <div className="px-4 lg:px-8 py-6">
+            <div>
+                <h1 className="text-2xl font-medium">
+                    Popular Anime (All Time)
+                </h1>
+            </div>
+            {/* Display a loading skeleton if the data is still being loaded */}
+            {loading ? (
+                <AnimeListSkeleton type="default" />
+            ) : (
+                // Display the list of popular anime
+                <AnimeList type="default" list={popularAnime} />
+            )}
+        </div>
+    );
 }
--- app/_components/trending.tsx
+++ app/_components/trending.tsx
@@ -19,152 +19,158 @@ import "keen-slider/keen-slider.min.css";
 const animation = { duration: 20000, easing: (t: number) => t };
 
 export default function Trending() {
-  const [trendingAnime, setTrendingAnime] = useState<AnilistResult[]>([]);
-  const [loading, setLoading] = useState(true);
+    const [trendingAnime, setTrendingAnime] = useState<AnilistResult[]>([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchTrendingAnime() {
-      try {
-        const response = await getTrendingAnime();
-        setTrendingAnime(response.results);
-      } catch (error) {
-        throw new Error("Error fetching trending anime");
-      } finally {
-        setLoading(false);
-      }
+    useEffect(() => {
+        async function fetchTrendingAnime() {
+            try {
+                const response = await getTrendingAnime();
+                setTrendingAnime(response.results);
+            } catch (error) {
+                throw new Error("Error fetching trending anime");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchTrendingAnime();
+    }, []);
+
+    const [ref] = useKeenSlider<HTMLDivElement>({
+        breakpoints: {
+            "(min-width: 0px)": {
+                slides: {
+                    perView: 2,
+                    spacing: 15,
+                },
+            },
+            "(min-width: 600px)": {
+                slides: {
+                    perView: 3,
+                    spacing: 15,
+                },
+            },
+            "(min-width: 1024px)": {
+                slides: {
+                    perView: 4,
+                    spacing: 15,
+                },
+            },
+            "(min-width: 1280px)": {
+                slides: {
+                    perView: 5,
+                    spacing: 15,
+                },
+            },
+            "(min-width: 1440px)": {
+                slides: {
+                    perView: 6,
+                    spacing: 15,
+                },
+            },
+            "(min-width: 1600px)": {
+                slides: {
+                    perView: 7,
+                    spacing: 15,
+                },
+            },
+        },
+        mode: "free",
+        renderMode: "precision",
+        drag: true,
+        loop: true,
+        created(s) {
+            s.moveToIdx(5, true, animation);
+        },
+        updated(s) {
+            s.moveToIdx(s.track.details.abs + 5, true, animation);
+        },
+        animationEnded(s) {
+            s.moveToIdx(s.track.details.abs + 5, true, animation);
+        },
+    });
+
+    if (loading) {
+        return <TrendingSkeleton />;
     }
 
-    fetchTrendingAnime();
-  }, []);
-
-  const [ref] = useKeenSlider<HTMLDivElement>({
-    breakpoints: {
-      "(min-width: 0px)": {
-        slides: {
-          perView: 2,
-          spacing: 15,
-        },
-      },
-      "(min-width: 600px)": {
-        slides: {
-          perView: 3,
-          spacing: 15,
-        },
-      },
-      "(min-width: 1024px)": {
-        slides: {
-          perView: 4,
-          spacing: 15,
-        },
-      },
-      "(min-width: 1280px)": {
-        slides: {
-          perView: 5,
-          spacing: 15,
-        },
-      },
-      "(min-width: 1440px)": {
-        slides: {
-          perView: 6,
-          spacing: 15,
-        },
-      },
-      "(min-width: 1600px)": {
-        slides: {
-          perView: 7,
-          spacing: 15,
-        },
-      },
-    },
-    mode: "free",
-    renderMode: "precision",
-    drag: true,
-    loop: true,
-    created(s) {
-      s.moveToIdx(5, true, animation);
-    },
-    updated(s) {
-      s.moveToIdx(s.track.details.abs + 5, true, animation);
-    },
-    animationEnded(s) {
-      s.moveToIdx(s.track.details.abs + 5, true, animation);
-    },
-  });
-
-  if (loading) {
-    return <TrendingSkeleton />;
-  }
-
-  return (
-    <div className="px-4 lg:px-8 py-6">
-      <div>
-        <h1 className="text-2xl font-medium">Trending Now</h1>
-        <p className="text-sm text-muted-foreground">Popular animes for you.</p>
-      </div>
-      <div className="mt-6 keen-slider relative" ref={ref}>
-        {trendingAnime.map((anime, index) => (
-          <div
-            key={index}
-            className="keen-slider__slide group overflow-hidden shadow-lg p-2 rounded-lg"
-          >
-            <Link href={`/anime/${anime.id}`}>
-              <Image
-                loading="lazy"
-                src={anime.image || "default.png"}
-                alt={
-                  anime.title.english || anime.title.userPreferred || "No Title"
-                }
-                width={460}
-                height={650}
-                className="object-cover w-full h-[300px] lg:h-[350px] rounded-lg hover:scale-105 transition-all duration-300 hover:opacity-80"
-              />
-              <div className="py-3 flex-1 flex flex-col">
-                <h2 className="text-lg font-semibold leading-6">
-                  {(
-                    anime.title.english ||
-                    anime.title.userPreferred ||
-                    "No Title"
-                  ).length > 40
-                    ? (
-                        anime.title.english ||
-                        anime.title.userPreferred ||
-                        "No Title"
-                      ).slice(0, 37) + "..."
-                    : anime.title.english ||
-                      anime.title.userPreferred ||
-                      "No Title"}
-                </h2>
-                <p className="text-sm text-muted-foreground mt-1">
-                  {anime.releaseDate} • {anime.status}
+    return (
+        <div className="px-4 lg:px-8 py-6">
+            <div>
+                <h1 className="text-2xl font-medium">Trending Now</h1>
+                <p className="text-sm text-muted-foreground">
+                    Popular animes for you.
                 </p>
-              </div>
-            </Link>
-          </div>
-        ))}
-        <div className="absolute inset-y-0 left-0 w-24 bg-gradient-to-r from-background to-transparent pointer-events-none opacity-65"></div>
-        <div className="absolute inset-y-0 right-0 w-24 bg-gradient-to-l from-background to-transparent pointer-events-none opacity-65"></div>
-      </div>
-    </div>
-  );
+            </div>
+            <div className="mt-6 keen-slider relative" ref={ref}>
+                {trendingAnime.map((anime, index) => (
+                    <div
+                        key={index}
+                        className="keen-slider__slide group overflow-hidden shadow-lg p-2 rounded-lg"
+                    >
+                        <Link href={`/anime/${anime.id}`}>
+                            <Image
+                                loading="lazy"
+                                src={anime.image || "default.png"}
+                                alt={
+                                    anime.title.english ||
+                                    anime.title.userPreferred ||
+                                    "No Title"
+                                }
+                                width={460}
+                                height={650}
+                                className="object-cover w-full h-[300px] lg:h-[350px] rounded-lg hover:scale-105 transition-all duration-300 hover:opacity-80"
+                            />
+                            <div className="py-3 flex-1 flex flex-col">
+                                <h2 className="text-lg font-semibold leading-6">
+                                    {(
+                                        anime.title.english ||
+                                        anime.title.userPreferred ||
+                                        "No Title"
+                                    ).length > 40
+                                        ? (
+                                              anime.title.english ||
+                                              anime.title.userPreferred ||
+                                              "No Title"
+                                          ).slice(0, 37) + "..."
+                                        : anime.title.english ||
+                                          anime.title.userPreferred ||
+                                          "No Title"}
+                                </h2>
+                                <p className="text-sm text-muted-foreground mt-1">
+                                    {anime.releaseDate} • {anime.status}
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+                <div className="absolute inset-y-0 left-0 w-24 bg-gradient-to-r from-background to-transparent pointer-events-none opacity-65"></div>
+                <div className="absolute inset-y-0 right-0 w-24 bg-gradient-to-l from-background to-transparent pointer-events-none opacity-65"></div>
+            </div>
+        </div>
+    );
 }
 
 function TrendingSkeleton() {
-  return (
-    <div className="px-4 lg:px-8 py-6">
-      <div>
-        <h1 className="text-2xl font-medium">Trending Now</h1>
-        <p className="text-sm text-muted-foreground">Popular animes for you.</p>
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 mt-6">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <div
-            key={index}
-            className="group overflow-hidden rounded-lg shadow-lg h-[350px]"
-          >
-            <Skeleton className="w-full h-full" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="px-4 lg:px-8 py-6">
+            <div>
+                <h1 className="text-2xl font-medium">Trending Now</h1>
+                <p className="text-sm text-muted-foreground">
+                    Popular anime this season
+                </p>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 mt-6">
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <div
+                        key={index}
+                        className="group overflow-hidden rounded-lg shadow-lg h-[350px]"
+                    >
+                        <Skeleton className="w-full h-full" />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
