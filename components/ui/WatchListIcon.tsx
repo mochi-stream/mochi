0c3d4d6ee2d7e55f5b18418cc8d50f6a25cf10d7
Dave <davenlu1989@gmail.com>
2024-07-23 22:06:59 +0900

    chore: realign other files to have 2 tabs

--- components/ui/WatchListIcon.tsx
+++ components/ui/WatchListIcon.tsx
@@ -1,21 +1,21 @@
 import { CirclePlusIcon } from "lucide-react";
 
 const WatchListIcon = ({
-    id,
-    addToWatchList,
+  id,
+  addToWatchList,
 }: {
-    id: string;
-    addToWatchList: (
-        e: React.MouseEvent<SVGSVGElement, MouseEvent>,
-        id: string
-    ) => void;
+  id: string;
+  addToWatchList: (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+    id: string
+  ) => void;
 }) => {
-    return (
-        <CirclePlusIcon
-            onClick={(e) => addToWatchList(e, id)}
-            className="drop-shadow hover:text-purple-800"
-        ></CirclePlusIcon>
-    );
+  return (
+    <CirclePlusIcon
+      onClick={(e) => addToWatchList(e, id)}
+      className="drop-shadow hover:text-purple-800"
+    ></CirclePlusIcon>
+  );
 };
 
 export default WatchListIcon;
