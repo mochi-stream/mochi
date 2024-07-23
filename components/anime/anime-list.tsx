b0cc812d399daded7f50af8d108724490a53e858
Dave <davenlu1989@gmail.com>
2024-07-23 22:05:32 +0900

    chore: re-align prettier tabs

--- components/anime/anime-list.tsx
+++ components/anime/anime-list.tsx
@@ -18,8 +18,8 @@ import { TvMinimal } from "lucide-react";
  */
 
 interface SearchResultsProps {
-    type: "default" | "search";
-    list: AnilistResult[];
+  type: "default" | "search";
+  list: AnilistResult[];
 }
 
 /**
@@ -29,15 +29,15 @@ interface SearchResultsProps {
  * @returns {string} - The CSS class name.
  */
 const listVariants = cva("mt-6 grid grid-cols-2 gap-2 lg:gap-4 select-none", {
-    variants: {
-        type: {
-            default: "sm:grid-cols-4 lg:grid-cols-5",
-            search: "sm:grid-cols-2 lg:grid-cols-4",
-        },
+  variants: {
+    type: {
+      default: "sm:grid-cols-4 lg:grid-cols-5",
+      search: "sm:grid-cols-2 lg:grid-cols-4",
     },
-    defaultVariants: {
-        type: "default",
-    },
+  },
+  defaultVariants: {
+    type: "default",
+  },
 });
 
 /**
@@ -47,94 +47,90 @@ const listVariants = cva("mt-6 grid grid-cols-2 gap-2 lg:gap-4 select-none", {
  * @returns {JSX.Element} - The rendered component.
  */
 export function AnimeList({ type, list }: SearchResultsProps) {
-    const addToWatchListHandler = (
-        e: React.MouseEvent<SVGSVGElement, MouseEvent>,
-        id: string
-    ) => {
-        e.preventDefault();
-        console.log("TODO: Adding", id, "to watch list");
+  const addToWatchListHandler = (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+    id: string
+  ) => {
+    e.preventDefault();
+    console.log("TODO: Adding", id, "to watch list");
 
-        // TODO: Save watchlist into DB
-    };
-    return (
-        <div className={cn(listVariants({ type }))}>
-            {/* Map over the anime list and render each anime component */}
-            {list.map((anime, index) => (
-                <div
-                    key={index}
-                    className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
-                >
-                    <Link href={`/anime/${anime.id}`}>
-                        <div className="relative">
-                            {/* Render the anime image */}
-                            <Image
-                                loading="lazy"
-                                src={anime.image || "default.png"}
-                                alt={
-                                    anime.title.english ||
-                                    anime.title.userPreferred ||
-                                    "No Title"
-                                }
-                                width={460}
-                                height={650}
-                                className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg transition-all hover:opacity-80"
-                            />
-                            {/* Render the anime details */}
-                            <div className="absolute flex w-11/12 justify-between top-2 left-2">
-                                <div className="flex">
-                                    <div
-                                        className={`bg-purple-800 text-white text-xs font-semibold  px-2 py-1 ${
-                                            anime.totalEpisodes
-                                                ? "rounded-tl rounded-bl"
-                                                : "rounded"
-                                        }`}
-                                    >
-                                        {/* Render the anime type */}
-                                        {(anime.type || "").replace(/_/g, " ")}
-                                    </div>
-                                    {/* Render the anime total episodes */}
-                                    {anime.totalEpisodes && (
-                                        <div className="bg-teal-600 text-white text-xs font-semibold rounded-tr rounded-br px-2 py-1 flex items-center">
-                                            <TvMinimal className="h-3 w-3 mr-1" />
-                                            {anime.totalEpisodes}
-                                        </div>
-                                    )}
-                                </div>
-                                <WatchListIcon
-                                    id={anime.id}
-                                    addToWatchList={addToWatchListHandler}
-                                ></WatchListIcon>
-                            </div>
-                            {/* Render the anime gradient overlay */}
-                            <div className="absolute inset-0 bg-gradient-to-t from-10% from-black via-[transparent] to-transparent opacity-70 rounded-lg pointer-events-none"></div>
-                        </div>
-                        <div className="absolute bottom-0 left-0 right-0 p-5 text-white">
-                            {/* Render the anime title */}
-                            <h2 className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5">
-                                {(
-                                    anime.title.english ||
-                                    anime.title.userPreferred ||
-                                    "No Title"
-                                ).length > 40
-                                    ? (
-                                          anime.title.english ||
-                                          anime.title.userPreferred ||
-                                          "No Title"
-                                      ).slice(0, 37) + "..."
-                                    : anime.title.english ||
-                                      anime.title.userPreferred ||
-                                      "No Title"}
-                            </h2>
-                            {/* Render the anime release date and status */}
-                            <p className="text-sm mt-1">
-                                {anime.releaseDate} • {anime.status}
-                            </p>
-                        </div>
-                    </Link>
+    // TODO: Save watchlist into DB
+  };
+  return (
+    <div className={cn(listVariants({ type }))}>
+      {/* Map over the anime list and render each anime component */}
+      {list.map((anime, index) => (
+        <div
+          key={index}
+          className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
+        >
+          <Link href={`/anime/${anime.id}`}>
+            <div className="relative">
+              {/* Render the anime image */}
+              <Image
+                loading="lazy"
+                src={anime.image || "default.png"}
+                alt={
+                  anime.title.english || anime.title.userPreferred || "No Title"
+                }
+                width={460}
+                height={650}
+                className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg transition-all hover:opacity-80"
+              />
+              {/* Render the anime details */}
+              <div className="absolute flex w-11/12 justify-between top-2 left-2">
+                <div className="flex">
+                  <div
+                    className={`bg-purple-800 text-white text-xs font-semibold  px-2 py-1 ${
+                      anime.totalEpisodes ? "rounded-tl rounded-bl" : "rounded"
+                    }`}
+                  >
+                    {/* Render the anime type */}
+                    {(anime.type || "").replace(/_/g, " ")}
+                  </div>
+                  {/* Render the anime total episodes */}
+                  {anime.totalEpisodes && (
+                    <div className="bg-teal-600 text-white text-xs font-semibold rounded-tr rounded-br px-2 py-1 flex items-center">
+                      <TvMinimal className="h-3 w-3 mr-1" />
+                      {anime.totalEpisodes}
+                    </div>
+                  )}
                 </div>
-            ))}
+                <WatchListIcon
+                  id={anime.id}
+                  addToWatchList={addToWatchListHandler}
+                ></WatchListIcon>
+              </div>
+              {/* Render the anime gradient overlay */}
+              <div className="absolute inset-0 bg-gradient-to-t from-10% from-black via-[transparent] to-transparent opacity-70 rounded-lg pointer-events-none"></div>
+            </div>
+            <div className="absolute bottom-0 left-0 right-0 p-5 text-white">
+              {/* Render the anime title */}
+              <h2 className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5">
+                {(
+                  anime.title.english ||
+                  anime.title.userPreferred ||
+                  "No Title"
+                ).length > 40
+                  ? (
+                      anime.title.english ||
+                      anime.title.userPreferred ||
+                      "No Title"
+                    ).slice(0, 37) + "..."
+                  : anime.title.english ||
+                    anime.title.userPreferred ||
+                    "No Title"}
+              </h2>
+              {/* Render the anime release date and status */}
+              <p className="text-sm mt-1">
+                {anime.releaseDate} • {anime.status}
+              </p>
+            </div>
+          </Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 /**
@@ -144,25 +140,25 @@ export function AnimeList({ type, list }: SearchResultsProps) {
  * @returns {JSX.Element} - The rendered component.
  */
 export function AnimeListSkeleton({ type }: Omit<SearchResultsProps, "list">) {
-    return (
-        <div className={cn(listVariants({ type }))}>
-            {/* Map over the number of anime results and render each skeleton */}
-            {Array(7)
-                .fill(0)
-                .map((_, index) => (
-                    <div
-                        key={index}
-                        className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
-                    >
-                        {/* Render the anime image skeleton */}
-                        <Skeleton className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg" />
-                        <div className="absolute bottom-0 left-0 right-0 p-5">
-                            {/* Render the anime details skeleton */}
-                            <Skeleton className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5" />
-                            <Skeleton className="text-sm mt-1" />
-                        </div>
-                    </div>
-                ))}
-        </div>
-    );
+  return (
+    <div className={cn(listVariants({ type }))}>
+      {/* Map over the number of anime results and render each skeleton */}
+      {Array(7)
+        .fill(0)
+        .map((_, index) => (
+          <div
+            key={index}
+            className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
+          >
+            {/* Render the anime image skeleton */}
+            <Skeleton className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg" />
+            <div className="absolute bottom-0 left-0 right-0 p-5">
+              {/* Render the anime details skeleton */}
+              <Skeleton className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5" />
+              <Skeleton className="text-sm mt-1" />
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
